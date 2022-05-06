f894e359751de028e01fd8795e102574772f005b
Jan Inge Dalsbø <JDALSB@equinor.com>
2022-05-06 12:29:00 +0200

    Docker suuport and zure-pipeline.yml - wip

--- src/Equinor.ProCoSys.AutoCutoffFunction/AutoCutoffTrigger.cs
+++ src/Equinor.ProCoSys.AutoCutoffFunction/AutoCutoffTrigger.cs
@@ -2,14 +2,18 @@ using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
-namespace Equinor.ProCoSys.AutoCutoffFunction
+namespace Equinor.ProCoSys.AutoCutoffFunction;
+
+public class AutoCutoffTrigger
 {
-    public class AutoCutoffTrigger
+    private ILogger logger;
+    private const string Schedule1 = "0 0 1 * * Mon"; // Each Monday night at 01:00
+    private const string Schedule2 = "*/10 * * * * *"; // Each 10'th second
+
+    [FunctionName("AutoCutoffTrigger")]
+    public void Run([TimerTrigger(Schedule2)]TimerInfo timerInfo, ILogger log)
     {
-        [FunctionName("AutoCutoffTrigger")]
-        public void Run([TimerTrigger("0 0 1 * * Mon")]TimerInfo timerInfo, ILogger log)
-        {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-        }
+        logger = log;
+        logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
     }
 }

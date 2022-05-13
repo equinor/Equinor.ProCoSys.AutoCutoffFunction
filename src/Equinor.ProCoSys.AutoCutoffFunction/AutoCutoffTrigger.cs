98d8c39af2c0918df106adbc8b252ad87c055c79
Jan Inge Dalsbø <JDALSB@equinor.com>
2022-05-13 15:10:16 +0200

    Codereview

--- src/Equinor.ProCoSys.AutoCutoffFunction/AutoCutoffTrigger.cs
+++ src/Equinor.ProCoSys.AutoCutoffFunction/AutoCutoffTrigger.cs
@@ -10,21 +10,19 @@ namespace Equinor.ProCoSys.AutoCutoffFunction
     public class AutoCutoffTrigger
     {
         private readonly IConfiguration _configuration;
-        private ILogger logger;
 
         public AutoCutoffTrigger(IConfiguration configuration) => _configuration = configuration;
 
         [FunctionName("AutoCutoffTrigger")]
         public async Task RunAsync([TimerTrigger("%Schedule%")] TimerInfo timerInfo, ILogger log)
         {
-            logger = log;
-            logger.LogInformation($"Starting AutoCutoffTrigger trigger at: {DateTime.Now}");
+            log.LogInformation($"Starting AutoCutoffTrigger trigger at: {DateTime.Now}");
 
             var pcsUrl = _configuration.GetValue<string>("PCSUrl");
-            logger.LogInformation($"PCSUrl: {pcsUrl}");
+            log.LogInformation($"PCSUrl: {pcsUrl}");
 
             var mainSecret = _configuration.GetValue<string>("MainSecret");
-            logger.LogInformation($"MainSecret: {mainSecret.Substring(0, 3)}xxxxxx");
+            log.LogInformation($"MainSecret: {mainSecret.Substring(0, 3)}xxxxxx");
 
             var url = $"{pcsUrl.TrimEnd('/')}/runjob/RunAllCutoffAnonymous?key={mainSecret}";
             var result = await AutoCutoffRunner.RunAsync(url);
@@ -34,7 +32,7 @@ namespace Equinor.ProCoSys.AutoCutoffFunction
                 throw new Exception($"AutoCutoffTrigger trigger didn't exit with expected code {HttpStatusCode.NoContent}. Got code {result}");
             }
 
-            logger.LogInformation($"Finished AutoCutoffTrigger trigger at: {DateTime.Now}.");
+            log.LogInformation($"Finished AutoCutoffTrigger trigger at: {DateTime.Now}.");
         }
     }
 }
